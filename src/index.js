3abb775b5f9c0211683faa58cef001a1fcd20473
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-07 07:50:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
   
+  if (number < 20) {
+    
+    return numsArr[number];
+    
+  }
+  
+  const ones = getDigitByPosition(number, 1);
+    
+  const tens = getDigitByPosition(number, 2);
+    
+  if (number < 100) {
+
+    return getTensStr(tens, ones);
+  
+  }
+  
+  const hundreds = getDigitByPosition(number, 3);
+      
+  return numsArr[hundreds] + ' hundred' + (tens || ones ? ' ' + getTensStr(tens, ones) : '');
+  
+}
+
+const numsArr = [
+  'zero',       'one',        'two',        'three',      'four', 
+  'five',       'six',        'seven',      'eight',      'nine',
+  'ten',        'eleven',     'twelve',     'thirteen',   'fourteen',
+  'fifteen',    'sixteen',    'seventeen',  'eighteen',   'nineteen'
+];
+  
+const tensArr = [
+  '',         '',         'twenty',   'thirty',   'forty',
+  'fifty',    'sixty',    'seventy',  'eighty',   'ninety'
+];
+
+function getDigitByPosition(num, pos) {
+  
+  const digitStr = String(num).split('').reverse()[pos - 1];
+    
+  const digitNum = Number(digitStr);
+  
+  return digitNum;
+  
+}
+  
+function getTensStr(tens, ones) {
+  
+  if (tens < 2) {
+    
+    return numsArr[tens * 10 + ones];
+    
+  }
+  
+  return tensArr[tens] + (ones ? ' ' + numsArr[ones] : '');
+  
 }
